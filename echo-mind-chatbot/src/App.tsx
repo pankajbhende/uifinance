97fd5efddadb04ccc38d37131537c0ad079232a1
pankajbhende <46356869+pankajbhende@users.noreply.github.com>
2025-07-23 00:55:42 +0530

    Update App.tsx

--- echo-mind-chatbot/src/App.tsx
+++ echo-mind-chatbot/src/App.tsx
@@ -2,6 +2,29 @@ import React, { useState } from 'react';
 import { Button, Typography, TextField, Checkbox, FormControlLabel, Box } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { fetchTaxInfo, AskRequest, AskResponse } from './api';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 const App: React.FC = () => {
   const { t } = useTranslation();
